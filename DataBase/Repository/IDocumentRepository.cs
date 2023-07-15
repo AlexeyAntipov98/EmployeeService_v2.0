e4513fc01151a9655b9c082113c5572c491605fa
AlexeyAntipov98 <52125568+AlexeyAntipov98@users.noreply.github.com>
2023-07-16 00:43:22 +0300

    Add documents-api for Employee

---
+++ DataBase/Repository/IDocumentRepository.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeService_v2._0.Models;
+
+namespace EmployeeService_v2._0.DataBase.Repository
+{
+    public interface IDocumentRepository
+    {
+        IEnumerable<Document> GetEmployeeDocuments(int employeeId);
+        public void CreateDocument(Document document);
+        public void DeleteDocumentByID(int id);
+    }
+}
---
+++ Models/Document.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeService_v2._0.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = "";
+        public string Serial { get; set; } = "";
+        public string Number { get; set; } = "";
+        public int EmployeeId { get; set; }
+    }
+}
